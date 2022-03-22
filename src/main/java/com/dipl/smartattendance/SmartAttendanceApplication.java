6bf4a44038ca7344095d5f2acaed9f75b0fd358a
DaffaBarin <daffabarin@gmail.com>
2022-03-22 15:57:07 +0700

    first commit

---
+++ src/main/java/com/dipl/smartattendance/SmartAttendanceApplication.java
@@ -0,0 +1,14 @@
+package com.dipl.smartattendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAttendanceApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Asiap");
+		SpringApplication.run(SmartAttendanceApplication.class, args);
+	}
+
+}
